dba6aa378526db4c2e3e24dbab8ee320e6b1f567
Bruno Natali <b010010010n@gmail.com>
2019-11-11 09:26:37 -0200

    Complete easyDay & easyHour events trigger implementation

--- src/EasyDateTimeEvent.php
+++ src/EasyDateTimeEvent.php
@@ -8,11 +8,17 @@ class EasyDateTimeEvent
 {
     Private $loop;
 
-    Private $dayFuncArr = [];
     Private $dayFuncArrIndex = 0; // Will increment in function and never will be zeroe
+    Private $dayFuncArr = [];
     Private $dayFuncVars = [
         'timer' => null,
         'today' => null
+    ];
+    Private $hourFuncArrIndex = 0; // Will increment in function and never will be zeroe
+    Private $hourFuncArr = [];
+    Private $hourFuncVars = [
+        'timer' => null,
+        'now' => null
     ];
 
     Private $t[
@@ -35,7 +41,7 @@ class EasyDateTimeEvent
 
             $time = $this->getSecondsTillDayEnd();
             echo "addDayTrigger ---> $time" . PHP_EOL;
-            
+
             $me = &$this;
             $this->addTimer($this->dayFuncTimer, $time, function () use ($me) {
                 if ($me->dayFuncVars['today'] === date("d") && (date("i") != 59 || date("s") < 59)) {
@@ -54,10 +60,45 @@ class EasyDateTimeEvent
         return $this->dayFuncArrIndex;
     }
 
+    Public function addHourTrigger(callable $func = null): int
+    {
+        if ($func !== null) $this->hourFuncArr[ ++$this->hourFuncArrIndex ] = $func;
+
+        if ($this->hourFuncVars['timer'] === null) {
+            $this->hourFuncVars['now'] = date("d");
+
+            $time = $this->getSecondsTillHourEnd();
+            echo "addHourTrigger ---> $time" . PHP_EOL;
+
+            $me = &$this;
+            $this->addTimer($this->dayFuncTimer, $time, function () use ($me) {
+                if ($me->hourFuncVars['now'] === date("H") && (date("i") != 59 || date("s") < 59)) {
+                    $me->addHourTrigger();
+                    return;
+                }
+                foreach ($me->hourFuncArr as $hourFunc) $hourFunc();
+                $me->addHourTrigger();
+            },
+            false);
+        } else {
+            $this->loop->cancelTimer($this->hourFuncVars['timer']);
+            $this->addHourTrigger();
+        }
+
+        return $this->hourFuncArrIndex;
+    }
+
     Public function removeDayTrigger(int $index): bool
     {
         if (!isset($this->dayFuncArr[$index])) return false;
         unset($this->dayFuncArr[$index]);
+        return true;
+    }
+
+    Public function removeHourTrigger(int $index): bool
+    {
+        if (!isset($this->hourFuncArr[$index])) return false;
+        unset($this->hourFuncArr[$index]);
         return true;
     }
 
@@ -67,6 +108,19 @@ class EasyDateTimeEvent
         $time = $t['day'] - (date("H") * $t['hour']) - (date("i") * $t['minute']) - date("s");
         $time += $now - time(); // calculate drift time
         return $time;
+    }
+
+    Public function getSecondsTillHourEnd(): int
+    {
+        $now = time();
+        $time = $t['hour'] - (date("i") * $t['minute']) - date("s");
+        $time += $now - time(); // calculate drift time
+        return $time;
+    }
+
+    Public function getSecondsTillMinuteEnd(): int
+    {
+        return $t['minute'] - date("s");
     }
 
     Private function addTimer(&$timer, float $time, callable $func, bool $periodic = true)
