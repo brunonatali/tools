a2c53b73fac823cf5a311b27851a6052a02ff947
Bruno Natali <b010010010n@gmail.com>
2019-11-12 18:01:16 -0200

    Add EasyStore to easy save files

--- src/EasyStorage.php
+++ src/EasyStorage.php
@@ -10,7 +10,6 @@ class EasyStorage implements EasyStorageInterface
     Private $fileFormart = self::AS_JSON;
 
     Private $saveList = [];
-    Private $saveListIndex = 0;
 
     Private $theSystem;
 
@@ -18,15 +17,72 @@ class EasyStorage implements EasyStorageInterface
     {
         if ($sysInteraction !== null) $this->theSystem = &$sysInteraction;
         else $this->theSystem = new SystemInteraction();
+
+        $me = &$this;
+        $this->theSystem->setFunctionOnAppAborted(function () {
+            $me->save();
+        });
     }
 
     Public function addArray(array &$array, string $name)
     {
-        $this->saveList[++$this->saveListIndex] =
+        $hash = $this->getHash($name);
+        $this->saveList[$hash] = &$array;
+
+        return $hash;
     }
 
-    Public function save()
-    {
 
+    Public function getArray(string $fileName = null, string $arrayName = null)
+    {
+        if ($arrayName !== null) $fileName = $this->getHash($arrayName, $fileName);
+
+        if (!file_exists(__DIR__ . '/' . $fileName)) return false;
+
+        $content = null;
+        if (($content = file_get_contents(__DIR__ . '/' . $fileName)) === null) return false;
+
+        if (!is_array($content = json_decode($content, true))) return false;
+
+        return $content;
+    }
+
+    Public function save(string $hash = null): bool
+    {
+        if ($hash !== null) {
+            return $this->saveToFile($hash);
+        } else {
+            $toReturn = false;
+            foreach ($this->saveList as $hash => $arr) {
+                $toReturn = $this->saveToFile($hash);
+            }
+        }
+    }
+
+    Private checkStoredFile()
+    {
+        //password_verify('rasmuslerdorf', $hash);
+    }
+
+    Private function getHash(string $arrayName, string $fileName = null)
+    {
+        if ($fileName === null) $fileName = $this->getFileName();
+
+        eval(self::IDENTIFIER_VAR . self::EQUAL . self::DEFAULT_IDENTIFIER . self::SEMICOLON . 'return password_hash(' . self::IDENTIFIER_VAR . ', PASSWORD_DEFAULT)' . self::SEMICOLON);
+    }
+
+    Private getFileName()
+    {
+        return basename(__FILE__, '.php');
+    }
+
+    Private saveToFile(string $hashFileName): bool
+    {
+        $fp = fopen(__DIR__ . '/' . $hashFileName, 'w');
+        fwrite($fp, json_encode($this->saveList[$hashFileName]));
+        fclose($fp);
+
+        if (!file_exists(__DIR__ . '/' . $hashFileName)) return false;
+        return true;
     }
 }
--- src/EasyStorageInterface.php
+++ src/EasyStorageInterface.php
@@ -5,4 +5,9 @@ namespace BrunoNatali\Tools;
 interface EasyStorageInterface
 {
     Const AS_JSON = 0x01;
+
+    Const IDENTIFIER_VAR = '$identifier';
+    Const DEFAULT_IDENTIFIER = '$fileName_$arrayName';
+    Const EQUAL = ' = ';
+    Const SEMICOLON = ';';
 }
