ffd3a5b11ad7840b643d732d0f933b87e35489aa
Bruno Natali <b010010010n@gmail.com>
2020-11-26 17:12:54 -0300

    Organized to better understand

--- src/Encapsulation.php
+++ src/Encapsulation.php
@@ -5,21 +5,23 @@ namespace BrunoNatali\Tools;
 class Encapsulation
 {
 
-    Public static function formatClassConfigs4InitializationPrint(array $args = []): string
+    public static function formatClassConfigs4InitializationPrint(array $args = []): string
     {
         if (!empty($args)) {
             $compose = "Started with configs:";
+
             foreach ($args as $key => $value) {
                 $compose .= PHP_EOL . "\t$key => ";
-                if (is_array($value)) {
+
+                if (\is_array($value)) {
                     $compose .= UsefulFunction::implodeRecursive($value);
-                } else if (is_bool($value)) {
+                } else if (\is_bool($value)) {
                     $compose .= ($value ? 'true' : 'false');
-                } else if (is_object($value)) {
-                    if (method_exists($value, '__toString'))
+                } else if (\is_object($value)) {
+                    if (\method_exists($value, '__toString'))
                         $compose .= (string)$value;
                     else 
-                        $compose .= get_class($value);
+                        $compose .= \get_class($value);
                 } else {
                     $compose .= $value;
                 }
@@ -27,6 +29,7 @@ class Encapsulation
         } else {
             $compose = "Started with no configs.";
         }
+        
         return $compose;
     }
 }
