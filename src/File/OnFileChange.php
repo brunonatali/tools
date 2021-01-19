b5920f7c5ef202ae085fdb9c1e3c562a5d8d7728
Bruno Natali <b010010010n@gmail.com>
2021-01-19 16:42:29 -0300

    Documented & implemented inotify

--- src/File/OnFileChange.php
+++ src/File/OnFileChange.php
@@ -2,105 +2,175 @@
 
 namespace BrunoNatali\Tools\File;
 
+use BrunoNatali\Inotify\Factory as Inotify;
 use BrunoNatali\Tools\OutSystem;
 
-use React\EventLoop\Factory;
 use React\EventLoop\LoopInterface;
 
 class OnFileChange implements OnFileChangeInterface
 {
+    /**
+     * @var EventLoop
+    */
     private $loop = null;
 
+    /**
+     * @var string Path and file name
+    */
     private $file = null;
 
-    private $call = null;
+    /**
+     * @var callable Triggered right after polling or new value from inotify
+    */
+    public $call = null;
 
+    /**
+     * @var Inotify
+    */
+    private $inotify = null;
+
+    /**
+     * @var array Stores app configuration
+    */
     private $config = [];
 
+    /**
+     * @var array Registered timers
+    */
     private $timers = [];
 
+    /**
+     * @var int|bool Buffer for filemtime() (Unix timestamp)
+    */
     private $lastModifiedDate = null;
 
+    /**
+     * @var int Stores current method to get file notification
+    */
     private $system = self::USE_SYSTEM_POLLING;
     
+    /**
+     * @var OutSystem
+    */
     protected $outSystem;
 
-    function __construct(...$configs)
+    /**
+     * This class was created to help monitor the change of attributes of a 
+     * file, ie access, writing and removal
+     * 
+     * @param string Path and file name to watch
+     * @param LoopInterface
+     * @param callable Function to call after file changed
+     * @param array Configurações possíveis são:
+     *  'client_name' => (string) Debug output app name,
+     *  'auto_start' => (bool) If will strt monitoring right after initialization,
+     *  'force_ppolling' => (bool) Force system to use polling method or if will try to use intify,
+     *  'polling_time' => (float) Time to polling if force_ppolling or inotify not instantiated,
+     *  'specific_attr' => (int|array) Provide an Inotify constant to filter specific attr change
+     *      NOTE 1. To specify more than one constant place each one in array. 
+     *      NOTE 2. 'specific_attr' has no effect when running on polling method
+    */
+    function __construct(string $filePathName, LoopInterface &$loop, callable $callback, array $config = [])
     {
-        /**
-         * Construct app configs
-        */
-        foreach ($configs as $config) 
-            if ($config instanceof LoopInterface)
-                $this->loop = $config;
-            else if (\is_array($config))
-                $this->config += $config;
-            else if (\is_string($config))
-                $this->file = $config;
-            else if (\is_callable($config))
-                $this->call = $config;
+        $this->loop = &$loop;
+        $this->file = $filePathName;
+        $this->call = $callback;
+        $this->config = $config;
 
-        if ($this->file == '')
+        if ($this->file == null)
             throw new \Exception("Provide a file name", self::ERROR_FILE_NAME_ABSENT);
             
-        if ($this->call === null)
+        if ($this->call == null)
             throw new \Exception("Provide a function to call OnFileChange", self::ERROR_FILE_CALL_ABSENT);
-        
-        // Declare unset parameters
+
+        if ($this->loop == null)
+            throw new \Exception("Provide a LoopInterface variable", self::ERROR_FILE_LOOP_ABSENT);
+
+        // Default configs
         $this->config += [
             'client_name' => 'FILE-X',
             'auto_start' => true,
-            'polling_time' => 1.0 // 1 second
+            'force_ppolling' => false,
+            'polling_time' => 1.0, 
+            'specific_attr' => false
         ];
 
-        if ($this->loop === null)
-            $this->loop = Factory::create();
-
-        $config = OutSystem::helpHandleAppName( 
-            $this->config,
-            [
-                "outSystemName" => $this->config['client_name']
-            ]
-        );
-        $this->outSystem = new OutSystem($config);
-
         /**
-         * Check inotify extension & try to load if is possible
+         * Instantiate a debug output system or declare an ghost
+         *  OutSystem class to do not crash the code 
         */
-        if (\extension_loaded('inotify') === false) {
-            if (\function_exists('dl'))
+        if (\class_exists('OutSystem')) {
+            $config = OutSystem::helpHandleAppName( 
+                $this->config,
+                [
+                    "outSystemName" => $this->config['client_name']
+                ]
+            );
+            $this->outSystem = new OutSystem($config);
+        } else {
+            eval('class OutSystem
+            {
+                const LEVEL_NOTICE = 0xFF;
+                public function stdout(...$discard) {}
+            }');
+            $this->outSystem = new OutSystem();
+        }
+
+        if (!$this->config['force_ppolling']) {
+            /**
+             * Check inotify extension & class declaration
+            */
+            if (\class_exists('Inotify'))
                 try {
-                    if (!\dl('inotify'))
-                        $this->outSystem->stdout(
-                            "[WARNING] Could not find 'inotify' extension. Consider install, to improve performance", 
-                            OutSystem::LEVEL_NOTICE
-                        );
-                    else
-                        $this->system = self::USE_SYSTEM_INOTIFY;
+                    $this->inotify = new Inotify($this->loop);
+                    $this->system = self::USE_SYSTEM_INOTIFY;
                 } catch (\Exception $e) {
-                    $this->outSystem->stdout(
-                        "[WARNING] Auto load 'inotify' extension fail. " . $e->getMessage() .
-                            "Consider load into php.ini, to improve performance", 
-                        OutSystem::LEVEL_NOTICE
-                    );
+                    switch ($e->getCode()) {
+                        case Inotify::EXCEPTION_EXTENSION_LOAD: 
+                            $this->outSystem->stdout(
+                                "[WARNING] Could not load 'inotify' extension. Consider install, to improve performance", 
+                                OutSystem::LEVEL_NOTICE
+                            );
+                            break;
+                        case Inotify::EXCEPTION_EXTENSION_INIT:
+                            $this->outSystem->stdout(
+                                "[WARNING] Error loading 'inotify'. System will be set to polling mode", 
+                                OutSystem::LEVEL_NOTICE
+                            );
+                            break;
+                    }
                 }
             else
                 $this->outSystem->stdout(
-                    "[WARNING] Consider load 'inotify' into php.ini, to improve performance", 
+                    "[WARNING] Install 'brunonatali/inotify' through composer to improve performance", 
                     OutSystem::LEVEL_NOTICE
                 );
-        } else {
-            $this->system = self::USE_SYSTEM_INOTIFY;
         }
 
         if ($this->config['auto_start'])
             $this->start();
     }
 
-    public function start()
+    public function start(): bool
     {
+        if (!$this->stop())
+            return false;
+
+        $toReturn = true;
+
         switch ($this->system) {
             case self::USE_SYSTEM_POLLING:
+                // Very first polling request
+                try {
+                    self::isFileChanged($this->file, $this->lastModifiedDate);
+                } catch (\Exception $e) {
+                    $this->outSystem->stdout(
+                        "Error reading file '$this->file': " . $e->getMessage(), 
+                        OutSystem::LEVEL_NOTICE
+                    );
+                    $toReturn = false;
+                }
+
                 $this->timers['polling_timer'] = $this->loop->addPeriodicTimer(
                     $this->config['polling_time'],
                     function () {
@@ -114,26 +184,40 @@ class OnFileChange implements OnFileChangeInterface
                                     OutSystem::LEVEL_NOTICE
                                 );
                             }
-                        } else {
-                            try {
-                                self::isFileChanged($this->file, $this->lastModifiedDate);
-                            } catch (\Exception $e) {
-                                $this->outSystem->stdout(
-                                    "Error reading file '$this->file': " . $e->getMessage(), 
-                                    OutSystem::LEVEL_NOTICE
-                                );
-                            }
                         }
                     }
                 );
                 break;
             case self::USE_SYSTEM_INOTIFY:
-                # code...
+                if ($this->config['specific_attr']) {
+                    if (\is_int($this->config['specific_attr'])) {
+                        $notify->on($this->config['specific_attr'], [$this, 'call']);
+                        return $notify->add($this->file, $this->config['specific_attr']);
+                    } else if (\is_array($this->config['specific_attr'])) {
+                        $mask = 0;
+                        foreach ($this->config['specific_attr'] as $flag) {
+                            $mask = $mask | $flag;
+                            $notify->on($flag, [$this, 'call']);
+                        }
+
+                        return $notify->add($this->file, $mask);
+                    } 
+                    
+                    return false;
+                } else {
+                    $notify->on('all', [$this, 'call']);
+                    return $notify->add($this->file, IN_ATTRIB | IN_MODIFY);
+                }
+                break;
+            default:
+                return false;
                 break;
         }
+
+        return $toReturn;
     }
 
-    public function stop()
+    public function stop(): bool
     {
         switch ($this->system) {
             case self::USE_SYSTEM_POLLING:
@@ -141,25 +225,41 @@ class OnFileChange implements OnFileChangeInterface
                     $this->loop->cancelTimer($this->timers['polling_timer']);
                 break;
             case self::USE_SYSTEM_INOTIFY:
-                # code...
+                return $notify->remove($this->file);
                 break;
         }
+
+        return true;
     }
 
-    public function setPollingTime(int $time):bool
+    public function setPollingTime(float $time): bool
     {
-        if (!isset($this->timers['polling_timer']) || $this->timers['polling_timer'] === null)
+        if ($this->system !== self::USE_SYSTEM_POLLING)
             return false;
 
-        $this->stop();
+        $restart = false;
+
+        if (isset($this->timers['polling_timer']) && $this->timers['polling_timer'] !== null) {
+            $restart = true;
+            if (!$this->stop())
+                return false;
+        }
 
         $this->config['polling_time'] = $time;
 
-        $this->start();
+        if ($restart)
+            return $this->start();
 
-        return null;
+        return true;
     }
 
+    /**
+     * Auxiliary function to this class to check if file has changed
+     * Could be called on demand to do a instant check
+     * 
+     * @param string $file File path and name
+     * @param int &$lastModifiedDate Unix timestamp from last modification check
+    */
     public static function isFileChanged(string $file, int &$lastModifiedDate = null): bool
     {
         if (!\file_exists($file))
--- src/File/OnFileChangeInterface.php
+++ src/File/OnFileChangeInterface.php
@@ -6,12 +6,31 @@ interface OnFileChangeInterface
 {
     const ERROR_FILE_NAME_ABSENT = 0x110;
     const ERROR_FILE_CALL_ABSENT = 0x111;
-    const ERROR_FILE_NOT_EXIST = 0x112;
+    const ERROR_FILE_LOOP_ABSENT = 0x112;
+    const ERROR_FILE_NOT_EXIST = 0x113;
 
     const USE_SYSTEM_INOTIFY = 0x50;
     const USE_SYSTEM_POLLING = 0x51;
 
-    public function start();
-    public function stop();
-    public function setPollingTime(int $time):bool;
+    /**
+     * Just start monitoring system
+     * 
+     * @return bool
+    */
+    public function start(): bool;
+    
+    /**
+     * Stop current file monitoring
+     * 
+     * @return bool
+    */
+    public function stop(): bool;
+
+    /**
+     * Configure / reconfigure polling time
+     * 
+     * @param float $time New polling time
+     * @return bool
+    */
+    public function setPollingTime(int $time): bool;
 }
